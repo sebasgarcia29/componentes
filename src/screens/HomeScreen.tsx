ea05f00b3816ca4b432354a4dd502109de083eea
sebasgarcia29 <s.garcia@iimoney.co>
2021-04-17 18:56:03 -0500

    first commit

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export const HomeScreen = () => {
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+      <Icon name="star" size={100} color="#900" />
+    </View>
+  );
+};
